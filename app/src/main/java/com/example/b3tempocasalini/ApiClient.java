97a722b37d35110f74ed24100497dd12760b718a
enzoc <enzocasa83@gmail.com>
2022-11-21 16:55:52 +0100

    Changement build

---
+++ app/src/main/java/com/example/b3tempocasalini/ApiClient.java
@@ -0,0 +1,31 @@
+package com.example.b3tempocasalini;
+
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+class ApiClient {
+
+    private static Retrofit retrofit = null;
+
+    static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://particulier.edf.fr")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+
+
+        return retrofit;
+    }
+
+}
