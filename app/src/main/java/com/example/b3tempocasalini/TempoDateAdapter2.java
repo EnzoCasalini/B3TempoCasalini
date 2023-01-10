b11c2d6999e1f11354f49674b753f9a3e96417e0
enzoc <enzocasa83@gmail.com>
2023-01-10 11:25:05 +0100

    Exercice 3 terminé

--- app/src/main/java/com/example/b3tempocasalini/TempoDateAdapter2.java
+++ app/src/main/java/com/example/b3tempocasalini/TempoDateAdapter2.java
@@ -1,5 +1,7 @@
 package com.example.b3tempocasalini;
 
+import static com.example.b3tempocasalini.Tools.formatTempoHistoryDate;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,7 +11,7 @@ import androidx.annotation.NonNull;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.b3tempocasalini.databinding.TempoDateItemBinding;
+import com.example.b3tempocasalini.databinding.TempoDateItem2Binding;
 
 import java.util.List;
 
@@ -30,16 +32,18 @@ public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.Te
     public TempoDateAdapter2.TempoDateViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // Create a new view, which defines the UI of the list item
 
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item, parent, false);
-        TempoDateItemBinding binding = TempoDateItemBinding.bind(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item_2, parent, false);
+        TempoDateItem2Binding binding = TempoDateItem2Binding.bind(v);
         return new TempoDateAdapter2.TempoDateViewHolder(binding);
     }
 
 
     @Override
     public void onBindViewHolder(@NonNull TempoDateAdapter2.TempoDateViewHolder holder, int position) {
-        holder.binding.dateTv.setText(localTempoDates.get(position).getDate());
-        holder.binding.colorFl.setBackgroundColor(ContextCompat.getColor(context, localTempoDates.get(position).getCouleur().getColorResId()));
+        holder.binding.dateValueTv.setText(formatTempoHistoryDate(localTempoDates.get(position).getDate()));
+        holder.binding.colorValueTv.setText(localTempoDates.get(position).getCouleur().getStringResId());
+        holder.binding.colorFl2.setBackgroundColor(ContextCompat.getColor(context, localTempoDates.get(position).getCouleur().getColorResId()));
+        holder.binding.spaceFl.setBackgroundColor(ContextCompat.getColor(context, R.color.gray_600));
     }
 
 
@@ -50,9 +54,9 @@ public class TempoDateAdapter2 extends RecyclerView.Adapter<TempoDateAdapter2.Te
 
 
     public class TempoDateViewHolder extends RecyclerView.ViewHolder { // ViewHolder's permettent de récupérer seulement les éléments de nos layouts dont on a besoin.
-        TempoDateItemBinding binding;
+        TempoDateItem2Binding binding;
 
-        public TempoDateViewHolder(@NonNull TempoDateItemBinding binding) {
+        public TempoDateViewHolder(@NonNull TempoDateItem2Binding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
